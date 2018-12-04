55306e5206cc040fd3379538f80a94bf2d58d140
Thomas Chen <tom.chen@am.sony.com>
2018-12-04 09:13:28 -0800

    remove keyboard and fix some bugs

--- tests/dummy/app/components/on-navkey/component.js
+++ tests/dummy/app/components/on-navkey/component.js
@@ -1,22 +1,26 @@
 import Component from '@ember/component';
 import { tryInvoke } from '@ember/utils';
-import { on } from '@ember/object/evented';
 import layout from './template';
-import { EKMixin, EKOnInsertMixin, keyPress } from 'ember-keyboard';
 
-const NAMES = {
-  ArrowDown: 'on-down',
-  ArrowUp: 'on-up',
-  ArrowLeft: 'on-left',
-  ArrowRight: 'on-right'
-};
+const KEY_MAP = {
+  "40": 'on-down',
+  "38": 'on-up',
+  "37": 'on-left',
+  "39": 'on-right',
+}
 
-const x = key => on(keyPress(key), function() { tryInvoke(this, NAMES[key], []) });
-export default Component.extend(EKMixin, EKOnInsertMixin, {
+export default Component.extend({
   layout,
   tagName: '',
-  onDown: x('ArrowDown'),
-  onUp: x('ArrowUp'),
-  onLeft: x('ArrowLeft'),
-  onRight: x('ArrowRight')
+  registerHandler(handler) {
+    document.addEventListener('keyup', handler)
+    return () => document.removeEventListener('keyup', handler)
+  },
+
+  keyHandler(event) {
+    const methodName = KEY_MAP[event.which] || 'NOOP';
+    tryInvoke(this, methodName, [event])
+  },
+
+  NOOP: () => {},
 });
