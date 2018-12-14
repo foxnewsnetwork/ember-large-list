1dafc8cb6d7b84a89ce2a03d946bbe20f7379dba
Thomas Chen <tom.chen@am.sony.com>
2018-12-14 14:07:24 -0800

    start using iterators and generators instead of slice

--- addon/utils/fn.js
+++ addon/utils/fn.js
@@ -1,8 +1,30 @@
+import { isArray } from '@ember/array';
+import { isBlank } from '@ember/utils';
+
 // BEGIN-SNIPPET addon|utils|fn
 export { get } from '@ember/object';
 
+export function skip(it, n = 0) {
+  for (let i = 0; i < n; i++) {
+    it.next();
+  }
+  return it;
+}
+
+export function* take(it, n = 6) {
+  for (let i = 0; i < n; i++) {
+    yield it.next();
+  }
+}
+
 export function slice(start, finish, xs) {
-  return (xs || []).slice(start, finish);
+  if (isArray(xs)) {
+    return xs.slice(start, finish);
+  } else if (isBlank(xs)) {
+    return [];
+  } else if (typeof xs[Symbol.iterator] === 'function') {
+    return take(skip(xs, start), finish - start);
+  }
 }
 
 export function* range(start = 0, end = 5, step = 1) {
